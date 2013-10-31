bd33b95770c42f5021da72d6caebd2a54b7cb21f
Karl Heinz Marbaise <info@soebes.de>
2013-10-31 20:48:18 +0100

    - Improved setup etc.

--- src/it/username/verify.groovy
+++ src/it/username/verify.groovy
@@ -5,5 +5,5 @@ String text = file.getText("utf-8");
 
 assert text =~ /\[DEBUG\] define property build\.environment\.username = "(.*)"/
 
-assert text.contains("[echo] build.environment.username=")
+assert text.contains("[INFO] build.environment.username=")
 return true;
